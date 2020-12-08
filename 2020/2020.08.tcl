011ffbed70d93cf111d9352c5ef2d8af38ee12d6
JeeCeetje <jochencloet@hotmail.com>
2020-12-08 09:43:05 +0100

    Day 8 question 2

--- 2020/2020.08.tcl
+++ 2020/2020.08.tcl
@@ -10,33 +10,36 @@ set input       [split [read [open $puzzleNr\_input.txt r]] \n]
 # --------------------------------------------------------------------------------
 puts $puzzleNr:a
 
-set nrOfInstructions        [llength $input]
-set executedInstructions    [lrepeat $nrOfInstructions 0]
+proc runInstructions {instructionList} {
+    set nrOfInstructions        [llength $instructionList]
+    set executedInstructions    [lrepeat $nrOfInstructions 0]
 
-set accumulatorValue        0
+    set accumulatorValue        0
 
-for {set i 0} {$i < $nrOfInstructions} {} {
-    if {[lindex $executedInstructions $i]} {
-        break;                                                          ;# Break if this instruction has already been executed; start of infinite loop
+    for {set i 0} {$i < $nrOfInstructions} {} {
+        if {[lindex $executedInstructions $i]} {
+            return "infinite $accumulatorValue"                                 ;# Break if this instruction has already been executed; start of infinite loop
+        }
+        lset executedInstructions $i 1                                          ;# Keep track of already executed instructions
+        regexp {(.+)\s(.+)} [lindex $instructionList $i] all operation argument ;# Analyze instruction; no checks, input is clean
+        switch $operation {
+            acc {
+                incr accumulatorValue $argument
+                incr i
+            }
+            jmp {
+                incr i $argument                                            
+            }
+            nop -
+            default {
+                incr i                                                          ;# Default should not be needed (clean input), nop falls through to this
+            }
+        }
     }
-    lset executedInstructions $i 1                                      ;# Keep track of already executed instructions
-    regexp {(.+)\s(.+)} [lindex $input $i] all operation argument       ;# Analyze instruction; no checks, input is clean
-    switch $operation {
-        acc {
-            incr accumulatorValue $argument
-            incr i
-        }
-        jmp {
-            incr i $argument                                            
-        }
-        nop -
-        default {
-            incr i                                                      ;# Default should not be needed (clean input), nop falls through to this
-        }
-    }
+    return "finite $accumulatorValue"                                           ;# Instructions are finite
 }
 
-puts $accumulatorValue
+puts [lindex [runInstructions $input] 1]
 
 
 # --------------------------------------------------------------------------------
@@ -44,6 +47,21 @@ puts $accumulatorValue
 # --------------------------------------------------------------------------------
 puts $puzzleNr:b
 
+for {set i [expr [llength $input] -1]} {$i >= 0} {incr i -1} {                  ;# Start searching from end of instructions...
+    if {[regexp {(nop|jmp)\s(.+)} [lindex $input $i] all operation argument]} {
+        set newInput $input                                                     ;# ...and try replacing a nop/jmp with jmp/nop
+        if {$operation == "nop"} {
+            lset newInput $i "jmp $argument"
+        } else {
+            lset newInput $i "nop $argument"
+        }
+        set result [runInstructions $newInput]                                  ;# Test if this yields a finite instruction list
+        if {[lindex $result 0] == "finite"} {
+            puts [lindex $result 1]
+            break;
+        }
+    }
+}
 
 
 
@@ -53,6 +71,8 @@ puts $puzzleNr:b
 
 # 2020.08:a
 # 1384
+# 2020.08:b
+# 761
 
 
 # --------------------------------------------------------------------------------
