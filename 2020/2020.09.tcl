fe569c586b457bf0038ae991a6af96f9c72d4c39
JeeCeetje <jochencloet@hotmail.com>
2020-12-09 14:11:09 +0100

    Day 9 question 2

--- 2020/2020.09.tcl
+++ 2020/2020.09.tcl
@@ -3,6 +3,7 @@
 # --------------------------------------------------------------------------------
 set puzzleNr    [file rootname [file tail [info script]]]
 set input       [split [read [open $puzzleNr\_input.txt r]] \n]
+# set input       [split [read [open $puzzleNr\_example.txt r]] \n]
 
 
 # --------------------------------------------------------------------------------
@@ -12,6 +13,9 @@ puts $puzzleNr:a
 
 set preambleLength  25
 set streamLength    [llength $input]
+
+set incorrectVal    ""
+set indexIncorVal   ""
 
 for {set i $preambleLength} {$i < $streamLength} {incr i} {
     set preamble [lsort [lrange $input [expr $i-$preambleLength] [expr $i-1]]]      ;# This is the sorted preamble list
@@ -28,10 +32,13 @@ for {set i $preambleLength} {$i < $streamLength} {incr i} {
         }
     }
     if {!$verified} {                                                               ;# If we didn't verify the value, we have a faulty one!
-        puts $value
+        set incorrectVal $value
+        set indexIncorVal $i
         break
     }
 }
+
+puts $incorrectVal
 
 
 # --------------------------------------------------------------------------------
@@ -39,6 +46,24 @@ for {set i $preambleLength} {$i < $streamLength} {incr i} {
 # --------------------------------------------------------------------------------
 puts $puzzleNr:b
 
+set found 0
+set contiguousSet ""
+
+for {set i [expr $indexIncorVal-1]} {$i > 0 && !$found} {incr i -1} {               ;# Run back from the index of the found result
+    set sum 0
+    set j   $i
+    while {$sum < $incorrectVal} {                                                  ;# Add the previous indexed values until we exceed the value...
+        incr sum [lindex $input $j]
+        if {$sum == $incorrectVal} {                                                ;# ... or we hit it spot on!
+            set found 1
+            set contiguousSet [lrange $input $j $i]
+        }
+        incr j -1
+    }
+}
+
+set sorted [lsort -integer $contiguousSet]
+puts [expr [lindex $sorted 0] + [lindex $sorted end]]
 
 
 
@@ -48,6 +73,8 @@ puts $puzzleNr:b
 
 # 2020.09:a
 # 1212510616
+# 2020.09:b
+# 171265123
 
 
 # --------------------------------------------------------------------------------
