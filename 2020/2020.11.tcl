a8876b423a2985773fb105cd644c1d28437fc1fd
JeeCeetje <jochencloet@hotmail.com>
2020-12-11 23:09:05 +0100

    Day 11 question 2 - Create function of input to reset it - Modify the runRules function to accept other conditions - Create new occupation function for question 2 - Time the execution because it's a bit sloooooowwwwwww

--- 2020/2020.11.tcl
+++ 2020/2020.11.tcl
@@ -10,18 +10,23 @@ set input       [split [read [open $puzzleNr\_input.txt r]] \n]
 # --------------------------------------------------------------------------------
 puts $puzzleNr:a
 
-set y 0
-foreach i $input {
-    set x 0
-    foreach j [split [regsub -all "L" $i 0] ""] {       ;# Save the seating plan in an matrix
-        set plan($x,$y) $j
-        incr x
-    }
-    incr y
-}
 
-set plan(x) [string length [lindex $input 0]]
-set plan(y) $y
+proc createInitialPlan {} {
+    global input
+    global plan
+    set y 0
+    foreach i $input {
+        set x 0
+        foreach j [split [regsub -all "L" $i 0] ""] {   ;# Save the seating plan in an matrix
+            set plan($x,$y) $j
+            incr x
+        }
+        incr y
+    }
+
+    set plan(x) [string length [lindex $input 0]]
+    set plan(y) $y
+}
 
 
 proc printSeatingPlan {} {                              ;# Print for visualisation
@@ -72,23 +77,23 @@ proc checkAdjacentOccupied {x y} {                      ;# Check occupation of a
 }
 
 
-proc runRules {} {                                      ;# Run the rules a single time
+proc runRules {occupationFunction occupationLimit} {    ;# Run the rules a single time
     global plan
     set changed 0
     for {set y 0} {$y < $plan(y)} {incr y} {
         for {set x 0} {$x < $plan(x)} {incr x} {
-            set occupation($x,$y) [checkAdjacentOccupied $x $y]
-        }
+            set occupation($x,$y) [$occupationFunction $x $y]   ;# Create an occupation plan before changing the seating plan,
+        }                                                       ;# otherwise, the changes affect the next seat already.
     }
     for {set y 0} {$y < $plan(y)} {incr y} {
         for {set x 0} {$x < $plan(x)} {incr x} {
-            if {$plan($x,$y) == 0} {
+            if {$plan($x,$y) == 0} {                    ;# Rule 1 : empty to seated
                 if {$occupation($x,$y) == 0} {
                     set plan($x,$y) 1
                     set changed 1
                 }
-            } elseif {$plan($x,$y) == 1} {
-                if {$occupation($x,$y) >= 4} {
+            } elseif {$plan($x,$y) == 1} {              ;# Rule 2 : seated to empty
+                if {$occupation($x,$y) >= $occupationLimit} {
                     set plan($x,$y) 0
                     set changed 1
                 }
@@ -99,8 +104,11 @@ proc runRules {} {                                      ;# Run the rules a singl
 }
 
 
-while {[runRules]} {}
-puts [countOccupied]
+createInitialPlan
+puts [time {
+    while {[runRules checkAdjacentOccupied 4]} {}
+    puts [countOccupied]
+}]
 
 
 # --------------------------------------------------------------------------------
@@ -109,6 +117,68 @@ puts [countOccupied]
 puts $puzzleNr:b
 
 
+proc checkSeenOccupied {x y} {                          ;# Check occupation of seen seats
+    global plan
+    set occupied 0
+    for {set dir 0} {$dir < 8} {incr dir} {             ;# Check the eight directions
+        set seen 0
+        for {set c 1} {$c < $plan(y) && !$seen} {incr c} {
+            switch $dir {
+                0 { # North
+                    set X $x
+                    set Y [expr $y - $c]
+                }
+                1 { # North-east
+                    set X [expr $x + $c]
+                    set Y [expr $y - $c]
+                }
+                2 { # East
+                    set X [expr $x + $c]
+                    set Y $y
+                }
+                3 { # South-east
+                    set X [expr $x + $c]
+                    set Y [expr $y + $c]
+                }
+                4 { # South
+                    set X $x
+                    set Y [expr $y + $c]
+                }
+                5 { # South-west
+                    set X [expr $x - $c]
+                    set Y [expr $y + $c]
+                }
+                6 { # West
+                    set X [expr $x - $c]
+                    set Y $y
+                }
+                7 { # North-west
+                    set X [expr $x - $c]
+                    set Y [expr $y - $c]
+                }
+            }
+            if {$X > -1       &&
+                $X < $plan(x) &&
+                $Y > -1       &&
+                $Y < $plan(y) } {                       ;# Check boundary conditions
+                if {$plan($X,$Y) != "."} {              ;# Look over floor spaces
+                    incr occupied $plan($X,$Y)
+                    set seen 1
+                }
+            } else {
+                set seen 1                              ;# If we reached the border, stop searching in that direction
+            }
+        }
+    }
+    return $occupied
+}
+
+
+createInitialPlan                                       ;# Reset initial plan, otherwise we continue on the plan of question 1 :-)
+puts [time {
+    while {[runRules checkSeenOccupied 5]} {}
+    puts [countOccupied]
+}]
 
 
 # --------------------------------------------------------------------------------
@@ -117,8 +187,14 @@ puts $puzzleNr:b
 
 # 2020.11:a
 # 2329
+# 22479398 microseconds per iteration
+# 2020.11:b
+# 2138
+# 21353928 microseconds per iteration
 
 
 # --------------------------------------------------------------------------------
 # Notes
 # --------------------------------------------------------------------------------
+
+# Both calculations take around 22 seconds to complete. Probably there is a faster way?
