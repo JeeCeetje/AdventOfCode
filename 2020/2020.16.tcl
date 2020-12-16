e0a1737a2cd9e70241497664d22a9013fe41b5d2
jeeceetje <jochencloet@hotmail.com>
2020-12-16 20:20:28 +0100

    Add some comments

--- 2020/2020.16.tcl
+++ 2020/2020.16.tcl
@@ -42,25 +42,25 @@ set nrOfValidNearbyTickets  0
 
 set sumInvalidFields        0
 
-foreach ticket $nearbyTickets {
+foreach ticket $nearbyTickets {                                                         ;# For every ticket...
     set validTicket true
-    foreach fieldValue $ticket {
+    foreach fieldValue $ticket {                                                        ;# ... take every field value...
         set validField false
-        for {set r 0} {$r < $nrOfRules} {incr r} {
+        for {set r 0} {$r < $nrOfRules} {incr r} {                                      ;# ... check all rules against it...
             if {(($rules($r,a1) <= $fieldValue) && ($fieldValue <= $rules($r,a2))) ||
                 (($rules($r,b1) <= $fieldValue) && ($fieldValue <= $rules($r,b2))) } {
-                set validField true
+                set validField true                                                     ;# ... stop checking the rules if th field value applies to one of them, then this field value is valid.
                 break
             }
         }
         if {!$validField} {
-            incr sumInvalidFields $fieldValue
+            incr sumInvalidFields $fieldValue                                           ;# If this field value didn't follow a single rule, then the ticket is false.
             set validTicket false
         }
     }
-    if {$validTicket} {
+    if {$validTicket} {                                                                 ;# Only keep the valid tickets but...
         set f 0
-        foreach fieldValue $ticket {
+        foreach fieldValue $ticket {                                                    ;# ... instead of saving them as tickets, we save lists of field values.
             lappend fieldValues($f) $fieldValue
             incr f
         }
@@ -77,36 +77,36 @@ puts $sumInvalidFields
 # --------------------------------------------------------------------------------
 puts $puzzleNr:b
 
-for {set r 0} {$r < $nrOfRules} {incr r} {
-    for {set f 0} {$f < $nrOfFields} {incr f} {
+for {set r 0} {$r < $nrOfRules} {incr r} {                                              ;# Run all rules...
+    for {set f 0} {$f < $nrOfFields} {incr f} {                                         ;# ... against all lists of field values.
         set ruleAppliesToAllFieldValues true
-        foreach fieldValue $fieldValues($f) {
+        foreach fieldValue $fieldValues($f) {                                           ;# If every field value of that list applies to the current rule...
             if {!((($rules($r,a1) <= $fieldValue) && ($fieldValue <= $rules($r,a2))) ||
                   (($rules($r,b1) <= $fieldValue) && ($fieldValue <= $rules($r,b2)))) } {
                 set ruleAppliesToAllFieldValues false
                 break
             }
         }
-        if {$ruleAppliesToAllFieldValues} {
-            lappend rules($r,fieldID) $f
+        if {$ruleAppliesToAllFieldValues} {                                             ;# ... then this field *COULD* be one for that rule...
+            lappend rules($r,fieldID) $f                                                ;# ... so add this field as a possible field for this rule.
         }
     }
 }
 
 
 set rulesHandled ""
-for {set i 0} {$i < $nrOfRules} {incr i} {
+for {set i 0} {$i < $nrOfRules} {incr i} {                                              ;# Find a uniqe rule for every field.
     set uniqueForRule -1
     set uniqueFieldID -1
-    for {set r 0} {$r < $nrOfRules} {incr r} {
-        if {[llength $rules($r,fieldID)] == 1 && [lsearch $rulesHandled $r] < 0} {
+    for {set r 0} {$r < $nrOfRules} {incr r} {                                          ;# Run over all rules...
+        if {[llength $rules($r,fieldID)] == 1 && [lsearch $rulesHandled $r] < 0} {      ;# ... and check how many fields follow this rule, if it's only one (and it's not handled yet), we have a winner!
             set uniqueForRule $r
             set uniqueFieldID $rules($r,fieldID)
             lappend rulesHandled $r
             break
         }
     }
-    for {set r 0} {$r < $nrOfRules} {incr r} {
+    for {set r 0} {$r < $nrOfRules} {incr r} {                                          ;# Now remove this field as possible field for the other rules.
         if {$r == $uniqueForRule} {
             continue
         }
@@ -116,7 +116,7 @@ for {set i 0} {$i < $nrOfRules} {incr i} {
 }
 
 set multResult 1
-for {set r 0} {$r < $nrOfRules} {incr r} {
+for {set r 0} {$r < $nrOfRules} {incr r} {                                              ;# Final step, for ever "departure" field, multiply the values of my ticket.
     if {[regexp {departure} $rules($r,fieldName) all]} {
         set multResult [expr $multResult * [lindex $myTicket $rules($r,fieldID)]]
     }
