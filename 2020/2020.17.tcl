e8588fff19c8defddbfa5d426ecdb14028a7ffd1
JeeCeetje <jochencloet@hotmail.com>
2020-12-18 16:27:52 +0100

    Day 17, question 2

--- 2020/2020.17.tcl
+++ 2020/2020.17.tcl
@@ -17,15 +17,16 @@ regsub -all {\#} $input "1" input                               ;# Replace all a
 set input [split $input \n]
 
 
-proc initializeCube {cycle {enableW 0}} {                   ;# Initialize an empty cube for this cycle
+proc initializeCube {cycle {enableW 0}} {                       ;# Initialize an empty cube for this cycle
     set toSize   [expr $::dimension + $cycle + 1]               ;# Add extra per cycle for growing cube, add extra for easier neighbour calculation in the next cycle...
                                                                 ;# ... if we go back a cycle, to get its state or neighbours state, it will exist
     set fromSize [expr 0 - ($cycle + 1)]
-    set w 0
-    for {set z 0} {$z <= [expr $cycle + 1]} {incr z} {          ;# Only the positive Z part as the negative Z is a mirror around the zero plane
-        for {set y $fromSize} {$y < $toSize} {incr y} {
-            for {set x $fromSize} {$x < $toSize} {incr x} {
-                set ::cube($cycle,$x,$y,$z,$w) 0
+    for {set w 0} {$w <= [expr $enableW*($cycle + 1)]} {incr w} {;# Only the positive W part as the negative W is a mirror around the Z plane
+        for {set z 0} {$z <= [expr $cycle + 1]} {incr z} {      ;# Only the positive Z part as the negative Z is a mirror around the zero plane
+            for {set y $fromSize} {$y < $toSize} {incr y} {
+                for {set x $fromSize} {$x < $toSize} {incr x} {
+                    set ::cube($cycle,$x,$y,$z,$w) 0
+                }
             }
         }
     }
@@ -45,16 +46,17 @@ proc initializeCube {cycle {enableW 0}} {                   ;# Initialize an emp
 }
 
 
-proc printCube {cycle {enableW 0}} {                        ;# For visualization only
+proc printCube {cycle {enableW 0}} {                            ;# For visualization only
     puts "\n\nAfter $cycle cycles:"
-    set w 0
-    for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {      ;# Do print the mirrored negative Z planes, see the abs($z) further!
-        puts "\n z=$z"
-        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
-            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
-                puts -nonewline " $::cube($cycle,$x,$y,[expr abs($z)],$w)"
+    for {set w [expr $enableW*(-1*$cycle)]} {$w <= [expr $enableW*$cycle]} {incr w} { ;# Do print the mirrored negative W planes, see the abs($w) further!
+        for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {  ;# Do print the mirrored negative Z planes, see the abs($z) further!
+            puts "\n z=$z, w=$w"
+            for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+                for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
+                    puts -nonewline " $::cube($cycle,$x,$y,[expr abs($z)],[expr abs($w)])"
+                }
+                puts ""
             }
-            puts ""
         }
     }
 }
@@ -62,43 +64,46 @@ proc printCube {cycle {enableW 0}} {                        ;# For visualization
 
 proc getNrOfActiveNeighbours {cycle X Y Z {W 0} {enableW 0}} {
     set sumActive 0
-    set w 0
-    set neighbourW $w
-    for {set z -1} {$z <= 1} {incr z} {
-        set neighbourZ [expr abs($Z+$z)]                        ;# For layer 0, layer -1 should become 1 (mirror)
-        if {$neighbourZ > [expr $cycle + 1]} {continue}
-        for {set y -1} {$y <= 1} {incr y} {
-            set neighbourY [expr $Y+$y]
-            if {($neighbourY < $::cube($cycle,fromSize)) || 
-                ($neighbourY > $::cube($cycle,toSize))} {continue}
-            for {set x -1} {$x <= 1} {incr x} {
-                set neighbourX [expr $X+$x]
-                if {($neighbourX < $::cube($cycle,fromSize)) || 
-                    ($neighbourX > $::cube($cycle,toSize))} {continue}
-                incr sumActive $::cube($cycle,$neighbourX,$neighbourY,$neighbourZ,$neighbourW)
+    for {set w [expr $enableW*-1]} {$w <= [expr $enableW*1]} {incr w} {
+        set neighbourW [expr abs($W+$w)]
+        if {$neighbourW > [expr $cycle + 1]} {continue}
+        for {set z -1} {$z <= 1} {incr z} {
+            set neighbourZ [expr abs($Z+$z)]                        ;# For layer 0, layer -1 should become 1 (mirror)
+            if {$neighbourZ > [expr $cycle + 1]} {continue}
+            for {set y -1} {$y <= 1} {incr y} {
+                set neighbourY [expr $Y+$y]
+                if {($neighbourY < $::cube($cycle,fromSize)) || 
+                    ($neighbourY > $::cube($cycle,toSize))} {continue}
+                for {set x -1} {$x <= 1} {incr x} {
+                    set neighbourX [expr $X+$x]
+                    if {($neighbourX < $::cube($cycle,fromSize)) || 
+                        ($neighbourX > $::cube($cycle,toSize))} {continue}
+                    incr sumActive $::cube($cycle,$neighbourX,$neighbourY,$neighbourZ,$neighbourW)
+                }
             }
         }
     }
-    set sumActive [expr $sumActive - $::cube($cycle,$X,$Y,$Z,$W)]  ;# Do not count yourself (instead of checking in the triple loop, just do a substraction here)
+    set sumActive [expr $sumActive - $::cube($cycle,$X,$Y,$Z,$W)] ;# Do not count yourself (instead of checking in the triple loop, just do a substraction here)
     return $sumActive
 }
 
 
 proc runCycle {cycle {enableW 0}} {
     if {$cycle == 0} {return}
-    set w 0
-    for {set z 0} {$z <= $cycle} {incr z} {
-        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
-            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
-                set ownState $::cube([expr $cycle-1],$x,$y,$z,$w)
-                set neighboursState [getNrOfActiveNeighbours [expr $cycle-1] $x $y $z]
-                if {$ownState == 1} {
-                    if {($neighboursState == 2) || ($neighboursState == 3)} {
-                        set ::cube($cycle,$x,$y,$z,$w) 1        ;# Only set the active states, no need to re-set inactive states (was done in initializeCube)
-                    }
-                } else {
-                    if {$neighboursState == 3} {
-                        set ::cube($cycle,$x,$y,$z,$w) 1
+    for {set w 0} {$w <= [expr $enableW*$cycle]} {incr w} {
+        for {set z 0} {$z <= $cycle} {incr z} {
+            for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+                for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
+                    set ownState $::cube([expr $cycle-1],$x,$y,$z,$w)
+                    set neighboursState [getNrOfActiveNeighbours [expr $cycle-1] $x $y $z $w $enableW]
+                    if {$ownState == 1} {
+                        if {($neighboursState == 2) || ($neighboursState == 3)} {
+                            set ::cube($cycle,$x,$y,$z,$w) 1    ;# Only set the active states, no need to re-set inactive states (was done in initializeCube)
+                        }
+                    } else {
+                        if {$neighboursState == 3} {
+                            set ::cube($cycle,$x,$y,$z,$w) 1
+                        }
                     }
                 }
             }
@@ -109,11 +114,12 @@ proc runCycle {cycle {enableW 0}} {
 
 proc countActive {cycle {enableW 0}} {
     set sumActive 0
-    set w 0
-    for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {      ;# Do count the mirrored negative Z planes, see the abs($z) further!
-        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
-            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
-                incr sumActive $::cube($cycle,$x,$y,[expr abs($z)],$w)
+    for {set w [expr $enableW*(-1*$cycle)]} {$w <= [expr $enableW*$cycle]} {incr w} {   ;# Do count the mirrored negative W planes, see the abs($w) further!
+        for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {  ;# Do count the mirrored negative Z planes, see the abs($z) further!
+            for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+                for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
+                    incr sumActive $::cube($cycle,$x,$y,[expr abs($z)],[expr abs($w)])
+                }
             }
         }
     }
@@ -123,11 +129,11 @@ proc countActive {cycle {enableW 0}} {
 
 proc runCycles {nrOfCycles {enableW 0}} {
     for {set i 0} {$i <= $nrOfCycles} {incr i} {
-        initializeCube $i
-        runCycle $i
-        # printCube $i
+        initializeCube $i $enableW
+        runCycle $i $enableW
+        # printCube $i $enableW
     }
-    puts [countActive $nrOfCycles]
+    puts [countActive $nrOfCycles $enableW]
 }
 
 
@@ -139,7 +145,7 @@ runCycles 6
 # --------------------------------------------------------------------------------
 puts $puzzleNr:b
 
-
+puts [time {runCycles 6 1}]
 
 
 # --------------------------------------------------------------------------------
@@ -148,6 +154,9 @@ puts $puzzleNr:b
 
 # 2020.17:a
 # 269
+# 2020.17:b
+# 1380
+# 10323671 microseconds per iteration
 
 
 # --------------------------------------------------------------------------------
