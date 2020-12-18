1e974ba0d5ddb6a9ee702d1233eb7bdfb10297b6
JeeCeetje <jochencloet@hotmail.com>
2020-12-18 14:55:45 +0100

    Remove the "Half" from the naming, also do not use a local variable anymore

--- 2020/2020.17.tcl
+++ 2020/2020.17.tcl
@@ -18,28 +18,25 @@ set input [split $input \n]
 
 
 proc initializeCube {cycle} {                                   ;# Initialize an empty cube for this cycle
-    set toHalfSize   [expr $::dimension + $cycle + 1]           ;# Add extra per cycle for growing cube, add extra for easier neighbour calculation in the next cycle...
+    set toSize   [expr $::dimension + $cycle + 1]               ;# Add extra per cycle for growing cube, add extra for easier neighbour calculation in the next cycle...
                                                                 ;# ... if we go back a cycle, to get its state or neighbours state, it will exist
-    set fromHalfSize [expr 0 - ($cycle + 1)]
+    set fromSize [expr 0 - ($cycle + 1)]
     for {set z 0} {$z <= [expr $cycle + 1]} {incr z} {          ;# Only the positive Z part as the negative Z is a mirror around the zero plane
-        for {set y $fromHalfSize} {$y < $toHalfSize} {incr y} {
-            for {set x $fromHalfSize} {$x < $toHalfSize} {incr x} {
-                # if {$z == 0} {
-                # puts ::cube($cycle,$x,$y,$z)
-                # }
+        for {set y $fromSize} {$y < $toSize} {incr y} {
+            for {set x $fromSize} {$x < $toSize} {incr x} {
                 set ::cube($cycle,$x,$y,$z) 0
             }
         }
     }
-    set ::cube($cycle,toHalfSize)   [expr $toHalfSize  -1]      ;# Save that we can reuse it, but remove the extra one ;-)
-    set ::cube($cycle,fromHalfSize) [expr $fromHalfSize+1]
+    set ::cube($cycle,toSize)   [expr $toSize  -1]              ;# Save that we can reuse it, but remove the extra one ;-)
+    set ::cube($cycle,fromSize) [expr $fromSize+1]
     if {$cycle == 0} {                                          ;# For cycle zero, fill in with the input
-        set toHalfSize   $::dimension
-        set fromHalfSize 0
-        for {set y $fromHalfSize} {$y < $toHalfSize} {incr y} {
+        set toSize   $::dimension
+        set fromSize 0
+        for {set y $fromSize} {$y < $toSize} {incr y} {
             set values [lindex $::input $y]
             set values [split $values ""]
-            for {set x $fromHalfSize} {$x < $toHalfSize} {incr x} {
+            for {set x $fromSize} {$x < $toSize} {incr x} {
                 set ::cube($cycle,$x,$y,0) [lindex $values $x]
             }
         }
@@ -48,13 +45,11 @@ proc initializeCube {cycle} {                                   ;# Initialize an
 
 
 proc printCube {cycle} {                                        ;# For visualization only
-    set toHalfSize   $::cube($cycle,toHalfSize)
-    set fromHalfSize $::cube($cycle,fromHalfSize)
     puts "\n\nAfter $cycle cycles:"
     for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {      ;# Do print the mirrored negative Z planes, see the abs($z) further!
         puts "\n z=$z"
-        for {set y $fromHalfSize} {$y < $toHalfSize} {incr y} {
-            for {set x $fromHalfSize} {$x < $toHalfSize} {incr x} {
+        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
                 puts -nonewline " $::cube($cycle,$x,$y,[expr abs($z)])"
             }
             puts ""
@@ -70,12 +65,12 @@ proc getNrOfActiveNeighbours {cycle X Y Z} {
         if {$neighbourZ > [expr $cycle + 1]} {continue}
         for {set y -1} {$y <= 1} {incr y} {
             set neighbourY [expr $Y+$y]
-            if {($neighbourY < $::cube($cycle,fromHalfSize)) || 
-                ($neighbourY > $::cube($cycle,toHalfSize))} {continue}
+            if {($neighbourY < $::cube($cycle,fromSize)) || 
+                ($neighbourY > $::cube($cycle,toSize))} {continue}
             for {set x -1} {$x <= 1} {incr x} {
                 set neighbourX [expr $X+$x]
-                if {($neighbourX < $::cube($cycle,fromHalfSize)) || 
-                    ($neighbourX > $::cube($cycle,toHalfSize))} {continue}
+                if {($neighbourX < $::cube($cycle,fromSize)) || 
+                    ($neighbourX > $::cube($cycle,toSize))} {continue}
                 incr sumActive $::cube($cycle,$neighbourX,$neighbourY,$neighbourZ)
             }
         }
@@ -87,11 +82,9 @@ proc getNrOfActiveNeighbours {cycle X Y Z} {
 
 proc runCycle {cycle} {
     if {$cycle == 0} {return}
-    set toHalfSize   $::cube($cycle,toHalfSize)
-    set fromHalfSize $::cube($cycle,fromHalfSize)
     for {set z 0} {$z <= $cycle} {incr z} {
-        for {set y $fromHalfSize} {$y < $toHalfSize} {incr y} {
-            for {set x $fromHalfSize} {$x < $toHalfSize} {incr x} {
+        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
                 set ownState $::cube([expr $cycle-1],$x,$y,$z)
                 set neighboursState [getNrOfActiveNeighbours [expr $cycle-1] $x $y $z]
                 if {$ownState == 1} {
@@ -110,12 +103,10 @@ proc runCycle {cycle} {
 
 
 proc countActive {cycle} {
-    set toHalfSize   $::cube($cycle,toHalfSize)
-    set fromHalfSize $::cube($cycle,fromHalfSize)
     set sumActive 0
-    for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {  ;# Do count the mirrored negative Z planes, see the abs($z) further!
-        for {set y $fromHalfSize} {$y < $toHalfSize} {incr y} {
-            for {set x $fromHalfSize} {$x < $toHalfSize} {incr x} {
+    for {set z [expr -1*$cycle]} {$z <= $cycle} {incr z} {      ;# Do count the mirrored negative Z planes, see the abs($z) further!
+        for {set y $::cube($cycle,fromSize)} {$y < $::cube($cycle,toSize)} {incr y} {
+            for {set x $::cube($cycle,fromSize)} {$x < $::cube($cycle,toSize)} {incr x} {
                 incr sumActive $::cube($cycle,$x,$y,[expr abs($z)])
             }
         }
