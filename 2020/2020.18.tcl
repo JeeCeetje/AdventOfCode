8953136c8d3019f1e97fcbfb2346b8d02faa6ccc
JeeCeetje <jochencloet@hotmail.com>
2021-05-15 15:26:05 +0200

    Day 18, question 2 this was not committed yet, work in progress...

--- 2020/2020.18.tcl
+++ 2020/2020.18.tcl
@@ -56,30 +56,69 @@ puts $sum
 puts $puzzleNr:b
 
 set precedenceOrder ""
-lappend precedenceOrder {(\()(\d+)(\))}                     ;# Integer in brackets => supercool by putting e.g. (5) in expr, it will return 5
-lappend precedenceOrder {\((\d+)(\+)(\d+)\)}                ;# Sum in brackets => supercool by matching the + as a char and putting it in a var, we can use it in expr
-lappend precedenceOrder {\((\d+)(\*)(\d+)\)}                ;# Mult in brackets
-lappend precedenceOrder {(\d+)(\+)(\d+)}                    ;# Sum outside brackets
-lappend precedenceOrder {(\d+)(\*)(\d+)}                    ;# Mult outside brackets
+lappend precedenceOrder {(\d+)(\+)(\d+)}                    ;# Sum
+lappend precedenceOrder {(\d+)(\*)(\d+)}                    ;# Mult
 
 
-proc calculateAdvanced {expressionString} {                 ;# Do checks on multiple digits as the subresult will be larger than single digits
-    for {set i 0} {$i < [llength $::precedenceOrder]} {} {
+proc calculateInner {expressionString} {                    ;# Do checks on multiple digits as the subresult will be larger than single digits
+    for {set i 0} {$i < [llength $::precedenceOrder]} {incr i} {
         if {[regexp [lindex $::precedenceOrder $i] $expressionString all a op b]} {
+            puts $expressionString
             regsub [lindex $::precedenceOrder $i] $expressionString [expr $a $op $b] expressionString
+            puts $expressionString
             set i 0
-            # puts $expressionString
-        } else {
-            incr i
         }
     }
-    # puts ""
+    puts ""
     return $expressionString
 }
 
 
+# set nonIntegers         {\(|\)|\+|\*}
+# set precedenceOrder ""
+# # lappend precedenceOrder {\((\d+)(\+)(\d+)\)}                ;# Sum in brackets => supercool by matching the + as a char and putting it in a var, we can use it in expr
+# lappend precedenceOrder {(\d+)(\+)(\d+)}                    ;# Sum outside brackets
+# # lappend precedenceOrder {\((\d+)(\*)(\d+)\)}                ;# Mult in brackets
+# lappend precedenceOrder {(\d+)(\*)(\d+)}                    ;# Mult outside brackets
+# set brackets            {(\()(\d+)(\))}                     ;# Integer in brackets => supercool by putting e.g. (5) in expr, it will return 5
+
+
+# proc calculateAdvanced {expressionString} {                 ;# Do checks on multiple digits as the subresult will be larger than single digits
+    # while {[regexp $::nonIntegers $expressionString]} {     ;# As long as we have no result, keep looping
+        # for {set i 0} {$i < [llength $::precedenceOrder]} {incr i} {    ;# Try to solve as much as possible without removing brackets
+            # if {[regexp [lindex $::precedenceOrder $i] $expressionString all a op b]} {
+                # regsub [lindex $::precedenceOrder $i] $expressionString [expr $a $op $b] expressionString
+                # puts $expressionString
+                # set i 0
+            # }
+        # }
+        # while {[regexp $::brackets $expressionString all a op b]} {     ;# If all is exhausted, remove brackets around integers
+            # regsub $::brackets $expressionString [expr $a $op $b] expressionString
+            # puts $expressionString
+        # }
+    # }
+    # puts ""
+            # puts $expressionString
+    # return $expressionString
+# }
+
+set bracketExpression {\((\d+(\+|\*)[\d\+\*]+)\)}           ;# This regex finds an inner bracket expression
+
+proc calculateAdvanced {expressionString} {
+    set result 0
+    while {[regexp $::bracketExpression $expressionString all expression]} {        ;# Find inner expressions
+        regsub $::bracketExpression $expressionString ([calculateInner $expression]) expressionString    ;# Mark that the result of the expression is back put in quotes
+    }
+    # while {[regexp $::bracketExpression $expressionString all expression]} {        # First handle inner expressions
+    # }
+    return $result
+}
+
+
 set sum 0
+set j 0
 foreach i $input {
+    puts [incr j]
     incr sum [calculateAdvanced $i]
 }
 puts $sum
